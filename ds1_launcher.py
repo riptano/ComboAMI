6f45e6e1cc88454b48fcff24273a96f33e77d85f
Joaquin Casares <joaquin@datastax.com>
2011-10-17 17:47:31 -0500

    Trying to get rid of schema disagreements

--- ds1_launcher.py
+++ ds1_launcher.py
@@ -43,8 +43,9 @@ if internalip != conf.getConfig("AMI", "LeadingSeed"):
         nodetoolOut = subprocess.Popen(shlex.split(nodetoolStatement), stderr=subprocess.PIPE, stdout=subprocess.PIPE).stdout.read()
         if (nodetoolOut.lower().find("error") == -1 and nodetoolOut.lower().find("up") and len(nodetoolOut) > 0):
             logger.info("Seed node now online!")
+            time.sleep(15)
             break
-        time.sleep(5)
+        time.sleep(10)
         logger.info("Retrying seed node...")
 
 
