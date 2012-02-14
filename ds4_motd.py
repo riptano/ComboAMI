d1e86b1a8e2fc306fd356139967232f954e4d1ba
Joaquin Casares <joaquin@datastax.com>
2012-02-13 19:14:34 -0600

    Pass nodetool_out

--- ds4_motd.py
+++ ds4_motd.py
@@ -97,7 +97,7 @@ def check_for_one_up_node():
                 break
     return nodetool_out
 
-def waiting_for_full_cluster_to_launch():
+def waiting_for_full_cluster_to_launch(nodetool_out):
     start_time = time.time()
     while True:
         if nodetool_out.count("Up") == int(conf.get_config("Cassandra", "ClusterSize")):
