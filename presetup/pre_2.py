b0483d215a1f939ee69baace86df9c3fabcd3fa3
Joaquin Casares <joaquin@datastax.com>
2012-02-22 11:31:52 -0600

    Trailing space, clearer instructions, and more pre-installs.

--- presetup/pre_2.py
+++ presetup/pre_2.py
@@ -32,7 +32,7 @@ def install_software():
     time.sleep(5)
 
     # Install Java and other recommended tools
-    exe('sudo apt-get -y install sun-java6-jdk libjna-java htop emacs23-nox sysstat iftop binutils pssh pbzip2 xfsprogs zip unzip ruby openssl libopenssl-ruby curl maven2 ant liblzo2-dev ntp subversion')
+    exe('sudo apt-get -y install sun-java6-jdk libjna-java htop emacs23-nox sysstat iftop binutils pssh pbzip2 xfsprogs zip unzip ruby openssl libopenssl-ruby curl maven2 ant liblzo2-dev ntp subversion python-pip')
 
     # Install these for a much faster instance startup time
     exe('sudo apt-get -y install ca-certificates-java icedtea-6-jre-cacao java-common jsvc libavahi-client3 libavahi-common-data libavahi-common3 libcommons-daemon-java libcups2 libjna-java libjpeg62 liblcms1 libnspr4-0d libnss3-1d tzdata-java')
