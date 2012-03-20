389864de761d9752782276812491ba963d0a2762
Joaquin Casares <joaquin@datastax.com>
2012-03-19 20:14:30 -0500

    Clearer Java Instructions

--- presetup/pre_1.sh
+++ presetup/pre_1.sh
@@ -33,6 +33,10 @@ mv jdk-6u31-linux-x64.bin /opt/java/64/
 cd /opt/java/64
 chmod +x jdk*
 ./jdk*
+
+# Press Enter
+
+# Continue with Java
 sudo update-alternatives --install "/usr/bin/java" "java" "/opt/java/64/jdk1.6.0_31/bin/java" 1
 sudo update-alternatives --set java /opt/java/64/jdk1.6.0_31/bin/java
 exit
