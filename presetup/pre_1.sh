62e75e9040869b8445464bec3ae9cf57a241dfd3
Andrew Lundgren <lundgren@byu.net>
2014-01-17 19:02:58 -0700

    Update pre_1.sh
    Change the git clone to use https rather than git:// so that another port is not required for restricted outbound configurations.

--- presetup/pre_1.sh
+++ presetup/pre_1.sh
@@ -26,7 +26,7 @@ sudo apt-get -y install git
 git config --global color.ui auto
 git config --global color.diff auto
 git config --global color.status auto
-git clone git://github.com/riptano/ComboAMI.git datastax_ami
+git clone https://github.com/riptano/ComboAMI.git datastax_ami
 cd datastax_ami
 git checkout $(head -n 1 presetup/VERSION)
 
