ec251841e4116f18ef41c93305f69aed1bf20591
Mike Lococo <mike.lococo@datastax.com>
2015-06-02 14:49:18 -0400

    Preliminary packer build process

---
+++ build/README.md
@@ -0,0 +1,26 @@
+# Introduction
+It's periodically necessary to create fresh versions of ComboAMI. This is
+necessary to support new regions, deliver features that require changes to
+the update script, or to ship up-to-date versions of operating system software.
+It can also be useful for folks that want to customize the AMI for any reason.
+
+Packer is used to perform AMI creation, and replaces the "presetup" code
+previously used for AMI creation.
+
+# Prerequisites
+* Packer: https://www.packer.io/intro/getting-started/setup.html
+* An AWS account: Note the default packer config launches instances in every
+  region and saves many ami's, and will resource in AWS usage fees.
+
+# Usage
+```shell
+packer build -var "aws_access_key=exampleid" -var "aws_secret_key=examplekey" comboami-test.json```
+
+# Notes
+* When updating base images, refer to this list of official Ubuntu AMI's:
+  http://cloud-images.ubuntu.com/locator/ec2/
+
+# TODO
+* Add comment to packer json and document stripping them with a minifier.
+* Move from ebs to instance storage
+* Set block_device_mappings (I don't think we need launch_block_device_mappings)
---
+++ build/provision.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Enable xtrace for easier debugging. Echoes commands and variables.
+set -x
+
+# Exit on error so problems are obvious
+set -e
+
+# FIXME: Is the hs1.8xlarge boot issue fix in pre_1.sh still needed?
+# FIXME: Why do we need linux-2.8 for 12.04 (from pre_1.sh)
+
+### Install Software ###
+# cloud-init updates /etc/apt/sources.list at startup, and the update tends to
+# happen *after* ssh is up and breaks apt operations. Wait for it to
+# finish. For more details see:
+# https://github.com/mitchellh/packer/issues/41#issuecomment-21288589
+# https://github.com/mitchellh/packer/issues/41#issuecomment-108001183
+while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
+    sleep 1;
+done
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y --no-install-recommends install mdadm # ComboAMI dependency
+# FIXME: libopenssl-ruby is missing or has changed names, do we need it?
+# FIXME: Emacs24? Or just emacs?
+PACKAGES=(
+    git xfsprogs ntp software-properties-common # ComboAMI dependencies
+    libjna-java                                 # Cassandra dependencies
+    htop sysstat iftop dstat                    # System monitoring convenience
+    subversion maven2 ant make gcc binutils     # Compile Cassandra convenience
+    emacs23-nox pssh pbzip2 zip unzip s3cmd     # Handy tools convenience
+    curl tree python-pip ethtool
+    ruby
+    openssl
+    liblzo2-dev
+)
+sudo apt-get -y install --fix-missing ${PACKAGES[@]}
+
+### Install Java ###
+# Need to do this in a separate apt-get run in order to ensure
+# software-properties-common is installed which provides add-apt-repository
+#
+# For more details see:
+# http://www.webupd8.org/2012/01/install-oracle-java-jdk-7-in-ubuntu-via.html
+sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java7-installer oracle-java7-set-default
+sudo update-java-alternatives -s java-7-oracle
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+
+### Configure git ###
+git config --global color.ui auto
+git config --global color.diff auto
+git config --global color.status auto
+
+### Clone ComboAMI ###
+git clone https://github.com/riptano/ComboAMI.git datastax_ami
+cd datastax_ami
+git checkout ${COMBOAMI_VERSION}
+
+### Bash Profile ###
+# Login message displays status
+echo "python datastax_ami/ds4_motd.py" > /home/ubuntu/.profile
+
+# JAVA_HOME and HADOOP_HOME for the ubuntu user
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /home/ubuntu/.profile
+echo "export HADOOP_HOME=/usr/share/dse/hadoop" >> /home/ubuntu/.profile
+chmod 644 /home/ubuntu/.profile
+
+# JAVA_HOME and HADOOP_HOME for root
+sudo bash -c \
+    'echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle" > /root/.profile'
+sudo bash -c \
+    'echo "export HADOOP_HOME=/usr/share/dse/hadoop" >> /root/.profile'
+sudo chown root:root /root/.profile
+sudo chmod 644 /root/.profile
+
+### Sysctl ###
+sudo bash -c 'echo "vm.max_map_count = 131072 > /etc/sysctl.d/99-cassandra.conf"'
+
+### Limits ###
+cat << EOF > /tmp/cassandra-limits.conf
+cassandra - memlock unlimited
+cassandra - nofile 100000
+cassandra - nproc 32768
+cassandra - as unlimited
+root - memlock unlimited
+root - nofile 100000
+root - nproc 32768
+root - as unlimited
+EOF
+sudo mv /tmp/cassandra-limits.conf /etc/security/limits.d/cassandra.conf
+sudo chown root:root /etc/security/limits.d/cassandra.conf
+sudo chmod 644 /etc/security/limits.d/cassandra.conf
+
+### Init Script ###
+cat << EOF > /tmp/start-ami-script.sh
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: Start AMI Configurations on boot.
+# Description:       Enables AMI Configurations on startup.
+### END INIT INFO
+
+# Make sure variables get set
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+
+# Setup system properties
+echo 1 | sudo tee /proc/sys/vm/overcommit_memory
+
+# Clear old ami.log
+echo "\n======================================================\n" >> ami.log
+cd /home/ubuntu/datastax_ami
+python ds0_updater.py
+EOF
+sudo mv /tmp/start-ami-script.sh /etc/init.d/start-ami-script.sh
+sudo chown root:root /etc/init.d/start-ami-script.sh
+sudo chmod 755 /etc/init.d/start-ami-script.sh
+sudo update-rc.d -f start-ami-script.sh start 99 2 3 4 5 .
+
+### Clean up ###
+# FIXME: Probably most of this isn't needed but dupe the old logic for now
+rm -f ${HOME}/.ssh/*
+sudo rm -f /etc/ssh/ssh_host_dsa_key*
+sudo rm -f /etc/ssh/ssh_host_key*
+sudo rm -f /etc/ssh/ssh_host_rsa_key*
+sudo rm -rf /tmp/*
+sudo sudo apt-get clean
+sudo su -c 'history -c'
+history -c
+
+### Finished ###
+# Print a completion line. Due to 'set -e' at the top of this script, this is
+# only displayed if there are no errors.
+echo "Provisioning successful."
