3d9b8fa634a60c9109d283c5fdce45b400bb2ac7
Mike Lococo <mike.lococo@datastax.com>
2015-06-05 09:51:30 -0400

    Configure the combo-ami version to checkout at ami-build

--- build/provision.sh
+++ build/provision.sh
@@ -86,6 +86,8 @@ git config --global color.status auto
 ### Clone ComboAMI ###
 git clone https://github.com/riptano/ComboAMI.git datastax_ami
 cd datastax_ami
+# The COMBOAMI_VERSION environment variable is set by packer
+# and configured in the provisioner config
 git checkout ${COMBOAMI_VERSION}
 
 ### Bash Profile ###
