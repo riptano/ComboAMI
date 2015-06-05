0255ff7978bbbdfe4de956d202eae34a5c13e481
Mike Lococo <mike.lococo@datastax.com>
2015-06-05 10:19:28 -0400

    Trim trailing whitespace in logger.py

--- logger.py
+++ logger.py
@@ -22,7 +22,7 @@ def exe(command, log=True, expectError=False, shell=False):
     # Helper function to execute commands and print traces of the command and output for debugging/logging purposes
     process = subprocess.Popen(shlex.split(command), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=shell)
     read = process.communicate()
-    
+
     if log:
         # Print output on next line if it exists
         if len(read[0]) > 0:
@@ -32,7 +32,7 @@ def exe(command, log=True, expectError=False, shell=False):
                 appendLog('[EXEC:E] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command + ":\n" + read[1])
             else:
                 appendLog('[ERROR] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command + ":\n" + read[1])
-    
+
     if not log or (len(read[0]) == 0 and len(read[1]) == 0):
         appendLog('[EXEC] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command)
 
@@ -47,7 +47,7 @@ def pipe(command1, command2, log=True):
 
     if not log:
         read = ""
-    
+
     # Print output on next line if it exists
     if len(read) > 0:
         appendLog('[PIPE] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command1 + ' | ' + command2 + ":\n" + read)
@@ -55,11 +55,11 @@ def pipe(command1, command2, log=True):
         appendLog('[PIPE] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command1 + ' | ' + command2)
 
     output = p2.communicate()[0]
-    
+
     if log:
         if read and len(read) > 0:
             appendLog('[PIPE] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command1 + ' | ' + command2 + ":\n" + read)
- 
+
         if output and len(output[0]) > 0:
             appendLog('[PIPE] ' + time.strftime("%m/%d/%y-%H:%M:%S", time.localtime()) + ' ' + command1 + ' | ' + command2 + ":\n" + output[0])
         if output and len(output[1] > 0):
@@ -82,7 +82,7 @@ def error(infotext):
 def exception(filename):
     if type(sys.exc_info()[1]) == SystemExit:
         return
-    
+
     appendLog("[ERROR] Exception seen in %s:" % filename)
     import traceback
     appendLog(traceback.format_exc())
