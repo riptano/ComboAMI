5ce3a512b9bf5848607afeb850142d825a3ce4e8
Mike Lococo <mike.lococo@datastax.com>
2015-06-09 15:44:25 -0400

    gopackgo.sh now passes extra args through to packer build
    Handy for -debug and similar build-time tweaks

--- build/gopackgo.sh
+++ build/gopackgo.sh
@@ -5,13 +5,16 @@ set -e
 
 # Command line arguments
 if [ -z ${1} ]; then
-    echo "The first argument must be a packer config file or the string 'publish-official-images'."
+    echo "The first argument must be a packer config file "
+    echo "or the string 'publish-official-images'."
+    echo "Additional arguments are passed directly to the packer build command."
     exit 1
 fi
 
 if [ ${1} = "publish-official-images" ]; then
+    shift
     PACKER_CONF=official-image-config.json
-    ./official-image-config.py > official-image-config.json
+    ./official-image-config.py > ${PACKER_CONF}
 else
     PACKER_CONF=${1}
 fi
@@ -22,4 +25,4 @@ cat $PACKER_CONF | json_pp > ${PACKER_CONF_MINIFIED}
 
 # Packer Run
 packer validate -var-file=local.json ${PACKER_CONF_MINIFIED}
-packer build -var-file=local.json ${PACKER_CONF_MINIFIED}
+packer build -var-file=local.json $@ ${PACKER_CONF_MINIFIED}
