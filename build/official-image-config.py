e2960cbb8ea69dfdec9440f996c487a219834d4a
Mike Lococo <mike.lococo@datastax.com>
2015-06-15 15:46:39 -0400

    Tag 2.6-beta5

--- build/official-image-config.py
+++ build/official-image-config.py
@@ -15,7 +15,7 @@ import json
 
 # The tag, branch or other commit-ref that will be checked out and baked
 # into the AMI
-COMBOAMI_VERSION = "2.6-beta4"
+COMBOAMI_VERSION = "2.6-beta5"
 
 # The versions of Amazon AMI and API tools to download and install, used
 # for building the instance-store backed AMI's
